42571d03d0b80e1f095d0d9561fcd553280b92e8
Kevin Thompson <phlux@ewnix.net>
2023-08-28 09:03:11 -0500

    Typo

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import App from './App.vue'
 import router from './router.js'
 import store from './store.js'
 
-createApp(App).use(router).use(store)mount('#app')
+createApp(App).use(router).use(store).mount('#app')
